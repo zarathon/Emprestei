608ff85203f47264f39896933e9cdf594789645f
Zarathon Maia <zarathonmaia@gmail.com>
2012-04-01 18:47:31 -0300

    Arquivos iniciais via Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Emprestrei::Application.config.secret_token = '45c5455f74fa552d44e031e6c7e9b2fe1b19d6ff5eba5f74cdb432aab7492a4d09ffa25ccae1ebed476cdfcd4239c4a108c1791510563562537c7dcb1d70dc95'
